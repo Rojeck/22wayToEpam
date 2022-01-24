a4bd31285f215e7af04461f9e43e0019c6fdec4c
Bohdan <rojeck1999@gmail.com>
2022-01-25 00:15:12 +0300

    first commit

---
+++ projecttrain/script.js
@@ -0,0 +1,7 @@
+"use strict";
+let answer = [];
+answer[0] = prompt('як звати');
+answer[1] = prompt('кіко літ');
+answer[2] = prompt('де жевеш');
+console.log(`${answer[0]}-через рік буде працювати в епамі`);
+document.write(answer);
